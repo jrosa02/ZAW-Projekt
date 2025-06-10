d3a4800bae90c98e09b14f4dc17a4e4729479a30
romannowak9 <roman.nowak9@interia.pl>
2025-06-10 11:47:39 +0200

    filtering & exp. dec. frames

--- dataVisualization.py
+++ dataVisualization.py
@@ -10,9 +10,24 @@ event_dtype = np.dtype([
     ('t', np.uint64)
 ])
 
+
 class LanderData:
-    def __init__(self, npz_path):
+    '''
+    Read and proccess input events from given .npz file
+    @param: npz_path - path to input .npz file
+    '''
+    def __init__(self, npz_path : str, dvs_resolution : tuple = (200, 200),  # w zasadzie to ten zbiór danych symuluje DAVIS240, więc rozmiar ramki to powinno być 240x180, ale faktycznie w danych jest 200x200
+                 filter_data : bool = True, filter_t : float = 1 / 24, filter_k : int = 1, filter_size : int = 5):  # Filter params
         self.npz_path = npz_path
+        self.img_shape = dvs_resolution
+        
+        # Filtering parameters
+        self.__filter_data = filter_data
+        self.filter_t = filter_t if filter_data else None
+        self.filter_k = filter_k if filter_data else None
+        self.filter_size = filter_size if filter_data else None
+        
+        # Loading data to self.events, self.trajectory, self.timestamps and self.rangemeter
         self._load_data()
 
     def _load_data(self):
@@ -20,7 +35,27 @@ class LanderData:
         
         events_array = data["events"]
         events_np = np.array(events_array, dtype=event_dtype)
-        self.events = events_np
+        if not self.__filter_data:
+            self.events = events_np
+        else:  # Filtering events - not taking into account polarity of each event
+            self.__sae = np.zeros(self.img_shape, dtype=float)  # surface of active events - last timestamp for each event
+            self.signal_events_n = 0
+            filtered_events = []
+            for event in events_np:
+                xi, yi, ti = event['x'], event['y'], event['t']
+                # Omitting too frequent events
+                if ti - self.__sae[yi, xi] < 1e-5:
+                    continue
+                    
+                self.__sae[yi, xi] = ti
+                if not self.__is_ba(event):
+                    self.signal_events_n += 1
+                    filtered_events.append(event)
+            
+            self.noise_events_n = events_np.shape[0] - self.signal_events_n
+                
+            self.events = np.array(filtered_events, dtype=event_dtype)
+
         
         self.timestamps = data["timestamps"]
         self.trajectory = {
@@ -36,7 +71,10 @@ class LanderData:
 
     def summary(self):
         print(f"Loaded LanderData from: {self.npz_path}")
-        print(f"Events: {len(self.events)} entries")
+        if self.__filter_data and self.signal_events_n and self.noise_events_n:
+            print(f"Signal events: {self.signal_events_n}, Noise events: {self.noise_events_n}")
+        else:
+            print(f"Events: {len(self.events)} entries")
         print(f"Timestamps: {len(self.timestamps)} entries")
         print(f"Trajectory: {len(self.trajectory['position'])} entries")
         print(f"Rangemeter: {len(self.rangemeter['time'])} entries")
@@ -88,9 +126,10 @@ class LanderData:
         plt.show()
 
     
-    def display_event_frames(self, t_start=0, t_end=np.inf, tau=0.1, img_shape=(200, 200), wait=100):
+    def display_event_frames(self, t_start=0, t_end=np.inf, tau=0.1, wait=100, frame_type="standard"):
         """
         Displays event frames between t_start and t_end with temporal resolution `tau`.
+        - `frame_type`: str : "standard" or "exponential"
         """
         # Convert structured array to NumPy arrays
         ts = self.events["t"] * 1e-6  # Convert from µs to seconds
@@ -104,25 +143,26 @@ class LanderData:
         ts, xs, ys, ps = ts[mask], xs[mask], ys[mask], ps[mask]
 
         # Display frames in time slices of `tau`
-        temp_x, temp_y, temp_p = [], [], []
+        temp_x, temp_y, temp_p, temp_ts = [], [], [], []
         start_time = ts[0]
         frame_count = 0
 
         for i in range(len(ts)):
             t = ts[i]
+            temp_ts.append(ts[i])
             temp_x.append(xs[i])
             temp_y.append(ys[i])
             temp_p.append(ps[i])
 
             if t - start_time >= tau:
                 # Generate frame
-                frame = np.ones(img_shape, dtype=np.uint8) * 127
-                for x, y, p in zip(temp_x, temp_y, temp_p):
-                    if 0 <= y < img_shape[0] and 0 <= x < img_shape[1]:
-                        frame[y, x] = 255 if p == 1 else 0
-
+                if frame_type == "exponential":
+                    frame = self.__exponantial_decay_aggregation((temp_x, temp_y, temp_p, temp_ts))
+                else:  # Standard event frame
+                    frame = self.__event_frame_agregation((temp_x, temp_y, temp_p, temp_ts))
+       
                 # Show frame
-                frame = cv2.resize(frame, np.multiply(4, frame.shape), interpolation = cv2.INTER_LINEAR)
+                frame = cv2.resize(frame, np.multiply(4, frame.shape), interpolation = cv2.INTER_LINEAR)  # Resize, żeby było większe okno?
                 cv2.imshow("Event Frame", frame)
                 key = cv2.waitKey(wait)
                 if key == ord('q'):
@@ -136,3 +176,63 @@ class LanderData:
 
         print(f"\nDisplayed {frame_count} event frames.")
         cv2.destroyAllWindows()
+        
+    def __exponantial_decay_aggregation(self, events) -> np.ndarray:
+        '''
+        Type of event frame, which consider timestamp of event
+        - events: (events_x, events_y, events_polarity, events_ts) 
+        '''
+        frame = np.ones(self.img_shape, dtype=float) * 127.0
+
+        if events:
+            # Assuming thet events are in ascending order by timestamps
+            e_x, e_y, e_p, e_ts = events
+            max_ts = e_ts[-1]
+            delta_t = max_ts - e_ts[0]
+        
+            if delta_t <= 0:
+                # Too few events or timestamps are equal
+                for x, y, p in zip(e_x, e_y, e_p):
+                    if 0 <= y < self.img_shape[0] and 0 <= x < self.img_shape[1]:
+                        frame[y, x] = 255 if p == 1 else 0
+            else:
+                for x, y, p, ts in zip(e_x, e_y, e_p, e_ts):
+                    if 0 <= y < self.img_shape[0] and 0 <= x < self.img_shape[1]:
+                        decay = np.exp(-abs(max_ts - ts) / delta_t)
+                        value = (p * decay + 1) * 127.5
+                        frame[y, x] = np.clip(value, 0, 255)
+
+        return frame.astype(np.uint8)
+
+    def __event_frame_agregation(self, events) -> np.ndarray:
+        ''' 
+        Standard event frame 
+        @param events - (events_x, events_y, events_polarity, events_ts) 
+        '''
+        frame = np.ones(self.img_shape, dtype=np.uint8) * 127
+        e_x, e_y, e_p, _ = events
+        for x, y, p in zip(e_x, e_y, e_p):
+            if 0 <= y < self.img_shape[0] and 0 <= x < self.img_shape[1]:
+                frame[y, x] = 255 if p == 1 else 0
+
+        return frame
+
+
+    def __is_ba(self, event) -> bool:
+        '''
+        Background Activity Filter (BAF)
+        Zwraca True, jeśli zdarzenie wygląda na tło (szum), False jeśli to "prawdziwe" zdarzenie.
+        '''
+        x, y, ts = event['x'], event['y'], event['t']
+        offset = self.filter_size // 2
+
+        l_bound_y = max(0, y - offset)
+        u_bound_y = min(self.img_shape[0], y + offset + 1)
+        l_bound_x = max(0, x - offset)
+        u_bound_x = min(self.img_shape[1], x + offset + 1)
+
+        sae_window = self.__sae[l_bound_y:u_bound_y, l_bound_x:u_bound_x]
+
+        neighbors = np.sum((ts - sae_window) < self.filter_t) - 1  # odejmujemy 1, by pominąć samo zdarzenie
+
+        return neighbors < self.filter_k
---
+++ literature.md
@@ -0,0 +1,8 @@
+### Globally Optimal Event-Based Divergence Estimation for Ventral Landing
+- https://arxiv.org/pdf/2209.13168
+### Towards Bridging the Space Domain Gap for Satellite Pose Estimation using Event Sensing
+- https://arxiv.org/pdf/2209.11945
+### ON THE GENERATION OF A SYNTHETIC EVENT-BASED VISION DATASET FOR NAVIGATION AND LANDING
+- https://arxiv.org/pdf/2308.00394
+### Event-based spacecraft landing using time-to-contact
+- https://openaccess.thecvf.com/content/CVPR2021W/AI4Space/papers/Sikorski_Event-Based_Spacecraft_Landing_Using_Time-To-Contact_CVPRW_2021_paper.pdf
